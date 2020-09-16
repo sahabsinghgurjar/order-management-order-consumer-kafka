94407e16a2071ae12b1960f26209290e2ca0644d
Sahab Singh <sahabsinghgurjar@yahoo.com>
2020-09-16 16:39:47 -0500

    removed commented code

--- src/main/java/com/sahab/order/consumer/order/listeners/KafkaOrderListener.java
+++ src/main/java/com/sahab/order/consumer/order/listeners/KafkaOrderListener.java
@@ -4,11 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.sahab.order.common.model.OrderDetails;
@@ -30,48 +25,4 @@ public class KafkaOrderListener {
 		orderDetailsService.createOrder(orderDetailsVO);
 		  System.out.println(data);
 	  }
-
-	 /* @KafkaListener(
-	    topics = "order-in-topic,test", 
-	    groupId = "orderConsumerGroup")
-	  void commonListenerForMultipleTopics(String message) {
-	   // LOG.info("MultipleTopicListener - {}", message);
-		  System.out.println(message);
-	  }*/
-	
-/*
-	  @KafkaListener(
-	    groupId = "orderConsumerGroup",
-	    topicPartitions = @TopicPartition(
-	      topic = "order-in-topic",
-	      partitionOffsets = { @PartitionOffset(
-	        partition = "0", 
-	        initialOffset = "0") }))
-	  void listenToPartitionWithOffset(
-	    @Payload OrderDetails message,
-	    @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-	    @Header(KafkaHeaders.OFFSET) int offset) {
-		  LOG.info("Received message [{}] from partition-{} with offset-{}", 
-	        message, 
-	        partition, 
-	        offset);
-	  }*/
-	  
-
-	 /* @KafkaListener(topics = "order-in-topic")
-	  @SendTo("order-out-topic")
-	  String listenAndReply(String message) {
-	    LOG.info("ListenAndReply [{}]", message);
-	    return "This is a reply sent after receiving message";
-	  }*/
-	  
-	 // in case of multiple listeners , we can specifying which container factory to use.
-	  
-	  /*@KafkaListener(
-			    topics = "order-in-topic",
-			    groupId="orderConsumerGroup",
-			    containerFactory="userKafkaListenerContainerFactory")
-			  void listener(User user) {
-			    LOG.info("CustomUserListener [{}]", user);
-			  }*/
 }
