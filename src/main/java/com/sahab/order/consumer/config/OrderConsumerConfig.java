85278696ed2d772af5b9a39dc5be03af4537c3d6
Sahab Singh <sahabsinghgurjar@yahoo.com>
2020-09-16 16:36:40 -0500

    send msg to other topic after processing

--- src/main/java/com/sahab/order/consumer/config/OrderConsumerConfig.java
+++ src/main/java/com/sahab/order/consumer/config/OrderConsumerConfig.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,52 +15,90 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.sahab.order.common.model.OrderDetails;
-
 
 @Configuration
 @EnableKafka
 public class OrderConsumerConfig {
 
 	@Value("${bootstrap-servers}")
-	  private String bootstrapServers;
-	
+	private String bootstrapServers;
+
 	@Value("${consumer-groupId}")
-	  private String consumerGroupId;
+	private String consumerGroupId;
 
-	  @Bean
-	  public Map<String, Object> consumerConfigs() {
-	    Map<String, Object> props = new HashMap<>();
-	    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-	      bootstrapServers);
-	    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-	      StringDeserializer.class);
-	    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-	    		JsonDeserializer.class);
-	    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,true);
-	    props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-	    props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
-	    props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 10000);
-	    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-	    return props;
+	@Bean
+	public Map<String, Object> consumerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+		props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+		props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 10000);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		return props;
+	}
+
+	@Bean
+	public ConsumerFactory<String, String> rawDataconsumerFactory() {
+		Map<String, Object> config = consumerConfigs();
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId+1);
+		return new DefaultKafkaConsumerFactory<>(config);
+	}
+
+	@Bean
+	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> rawDataContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(rawDataconsumerFactory());
+		return factory;
+	}
+
+	@Bean
+	public ConsumerFactory<String, OrderDetails> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+	}
+
+	@Bean
+	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, OrderDetails>> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, OrderDetails> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		factory.setReplyTemplate(kafkaReplyTemplate());
+		factory.setRecordFilterStrategy(record -> record.value().getOrderName().contains("ignored"));
+		return factory;
+	}
+	
+/******************************************Reply template config********************/	
+
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put( 
+		            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
+		            StringSerializer.class); 
+		return props;
+	}
+
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+	@Bean
+	  public KafkaTemplate<String, String> kafkaReplyTemplate() {
+		  KafkaTemplate<String, String> kafkaTemplate= new KafkaTemplate<>(producerFactory());
+		  return kafkaTemplate;
 	  }
 
-	  @Bean
-	  public ConsumerFactory<String, OrderDetails> consumerFactory() {
-	    return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-	  }
-
-	  @Bean
-	  public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, OrderDetails>> kafkaListenerContainerFactory() {
-	    ConcurrentKafkaListenerContainerFactory<String, OrderDetails> factory =
-	      new ConcurrentKafkaListenerContainerFactory<>();
-	    factory.setConsumerFactory(consumerFactory());
-	    factory.setRecordFilterStrategy(record -> 
-	      record.value().getOrderName().contains("ignored"));
-	    return factory;
-	  }
-	  
 }
--- src/main/java/com/sahab/order/consumer/order/listeners/KafkaOrderListener.java
+++ src/main/java/com/sahab/order/consumer/order/listeners/KafkaOrderListener.java
@@ -4,11 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import com.sahab.order.common.model.OrderDetails;
@@ -21,57 +17,21 @@ public class KafkaOrderListener {
 	@Autowired
 	private OrderDetailsService orderDetailsService;
 	Logger LOG= LoggerFactory.getLogger(KafkaOrderListener.class);
-	@KafkaListener(topics = "order-in-topic")
-	  void listener(OrderDetails data) throws DuplicateOrderException {
+	@KafkaListener(topics = "order-in-topic",containerFactory="kafkaListenerContainerFactory")
+	  @SendTo("order-out-topic")
+	  String listener(OrderDetails data) throws DuplicateOrderException {
 		OrderDetailsVO orderDetailsVO=new OrderDetailsVO();
 		orderDetailsVO.setOrderId(data.getOrderId());
 		orderDetailsVO.setOrderName(data.getOrderName());
 		orderDetailsVO.setPrice(data.getPrice());
 		orderDetailsService.createOrder(orderDetailsVO);
 		  System.out.println(data);
+		  return "This is a reply sent after receiving message";
 	  }
 
-	 /* @KafkaListener(
-	    topics = "order-in-topic,test", 
-	    groupId = "orderConsumerGroup")
-	  void commonListenerForMultipleTopics(String message) {
-	   // LOG.info("MultipleTopicListener - {}", message);
-		  System.out.println(message);
-	  }*/
-	
-/*
-	  @KafkaListener(
-	    groupId = "orderConsumerGroup",
-	    topicPartitions = @TopicPartition(
-	      topic = "order-in-topic",
-	      partitionOffsets = { @PartitionOffset(
-	        partition = "0", 
-	        initialOffset = "0") }))
-	  void listenToPartitionWithOffset(
-	    @Payload OrderDetails message,
-	    @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-	    @Header(KafkaHeaders.OFFSET) int offset) {
-		  LOG.info("Received message [{}] from partition-{} with offset-{}", 
-	        message, 
-	        partition, 
-	        offset);
-	  }*/
-	  
+	@KafkaListener(topics = "order-out-topic", containerFactory="rawDataContainerFactory")
+	  void orderOutListener(String data) throws DuplicateOrderException {
+		System.out.println("Message received from out topic "+data);
+	  }
 
-	 /* @KafkaListener(topics = "order-in-topic")
-	  @SendTo("order-out-topic")
-	  String listenAndReply(String message) {
-	    LOG.info("ListenAndReply [{}]", message);
-	    return "This is a reply sent after receiving message";
-	  }*/
-	  
-	 // in case of multiple listeners , we can specifying which container factory to use.
-	  
-	  /*@KafkaListener(
-			    topics = "order-in-topic",
-			    groupId="orderConsumerGroup",
-			    containerFactory="userKafkaListenerContainerFactory")
-			  void listener(User user) {
-			    LOG.info("CustomUserListener [{}]", user);
-			  }*/
 }
